a251ae886c72534f64d5373a9a7262f79d541e9d
Var24 <66937295+Var24@users.noreply.github.com>
2023-07-04 14:31:48 +0000

    Added dynamic templates and API endpoint

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+baskets = [{
+  'ID': 1,
+  'title': 'Low Volatility',
+  'benchmark': 'BSE 100',
+  'Methodology': 'Volatility based weighting',
+  'No of constituents': '30'
+},
+{
+  'ID': 2,
+  'title': 'Momentum',
+  'benchmark': 'BSE 200',
+  'Methodology': 'Tilt based weighting',
+  'No of constituents': '30'
+},
+{
+  'ID': 3,
+  'title': 'High Quality',
+  'benchmark': 'BSE 100',
+  'Methodology': 'Tilt based weighting',
+  'No of constituents': '30'
+},
+{
+  'ID': 4,
+  'title': 'Dividend Income',
+  'benchmark': 'BSE 100',
+  'Methodology': 'Periodic capped free float Mcap',
+  'No of constituents': '50'           
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",portfolios=baskets)
+
+@app.route("/api/baskets")
+def list_baskets():
+  return jsonify(baskets)
 
 print(__name__)
 if __name__=="__main__":
