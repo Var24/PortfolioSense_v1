618e183c5f1050103cbd9d09ac4fa1f06c92b307
Var24 <66937295+Var24@users.noreply.github.com>
2023-07-19 07:42:32 +0000

    added database connection

--- app.py
+++ app.py
@@ -1,46 +1,22 @@
 from flask import Flask, render_template, jsonify
+from database import load_baskets_from_db
+
 
 app = Flask(__name__)
-
-baskets = [{
-  'ID': 1,
-  'title': 'Low Volatility',
-  'benchmark': 'BSE 200',
-  'Methodology': 'Volatility based weighting',
-  'No of constituents': '30'
-},
-{
-  'ID': 2,
-  'title': 'Momentum',
-  'benchmark': 'BSE 200',
-  'Methodology': 'Tilt based weighting',
-  'No of constituents': '30'
-},
-{
-  'ID': 3,
-  'title': 'High Quality',
-  'benchmark': 'BSE 100',
-  'Methodology': 'Tilt based weighting',
-  'No of constituents': '30'
-},
-{
-  'ID': 4,
-  'title': 'Dividend Income',
-  'benchmark': 'BSE 100',
-  'Methodology': 'Periodic capped free float Mcap',
-  'No of constituents': '50'           
-}]
-
+     
 
 @app.route("/")
-def hello_world():
+def hello_baskets():
+  baskets = load_baskets_from_db()
   return render_template("home.html",portfolios=baskets)
 
 @app.route("/api/baskets")
 def list_baskets():
+  baskets = load_baskets_from_db()
   return jsonify(baskets)
 
 print(__name__)
 if __name__=="__main__":
-  #print("inside the if now")
-  app.run(host="0.0.0.0",debug=True)
+  
+  #app.run(host="0.0.0.0",debug=True)
+  app.run(host = "0.0.0.0")
